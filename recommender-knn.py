dfdc81c7667dbc02baff62558957e040e622bc90
brendan1liu <brendanliu1@gmail.com>
2019-12-10 23:30:07 -0800

    fixed knn-rec

--- recommender-knn.py
+++ recommender-knn.py
@@ -1,10 +1,17 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 from sklearn import model_selection
 from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import csr_matrix
+from time import sleep
 import random
+
+print('A Simple KNN Recommender!\nDeveloped by: Brendan, Yerem, Josh and Elaine!')
+print('----------------------------------------------------------------------------------')
+print('----------------------------------------------------------------------------------')
+sleep(1.5)
 
 ##Read in data
 print('Reading in Data...')
@@ -27,18 +34,27 @@ for i in range(cold_read):
     idx = random.randint(0,99)
     init_joke = joketext.JokeText[idx]
     print(init_joke)
+    print('----------------------------------------------------------------------------------')
     com = input('Rate this from -10 to 10:')
+    com = float(com)/20 + 1.5
+    print('----------------------------------------------------------------------------------')
     new_user[idx] = com
 
+
 while True:
+    if sum(new_user != 0) == 100:
+        print('No More Jokes!')
+        break
     distances, indices = model_knn.kneighbors(new_user.reshape(1,-1), n_neighbors = 734)
     neighbor = np.mean(df.iloc[:,np.intersect1d(df.columns.values.astype(int),indices)], axis = 1)[new_user==0]
     rec = np.argmax(np.array(neighbor))
     rec_joke = joketext.JokeText[rec]
     print(rec_joke)
+    print('----------------------------------------------------------------------------------')
     com = input('Rate this from -10 to 10:')
+    print('----------------------------------------------------------------------------------')
     if len(com) == 0:
         break
-    new_user[rec] = float(com)
-    print(new_user)
+    com = float(com)/20 + 1.5
+    new_user[rec] = com
     
